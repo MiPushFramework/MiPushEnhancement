fe46b60404fae4f9c0c0b64ac34e2426c734a36c
Jim Wu <mlgmxyysd@meowcat.org>
2020-07-21 17:09:39 +0800

    Create stub

---
+++ app/src/main/java/android/os/FileUtils.java
@@ -0,0 +1,16 @@
+package android.os;
+
+/*
+ * Stub!
+ * Decompiled by MlgmXyysd.
+ */
+public class FileUtils {
+    public static final int S_IRUSR = 256;
+    public static final int S_IWUSR = 128;
+    public static final int S_IRGRP = 32;
+    public static final int S_IWGRP = 16;
+    public static final int S_IROTH = 4;
+    public static final int S_IWOTH = 2;
+
+    public static native int setPermissions(String path, int mode, int uid, int gid);
+}
