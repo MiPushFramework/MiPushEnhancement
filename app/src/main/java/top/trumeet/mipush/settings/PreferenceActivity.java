163d6523561b70c3786f83528b35bac795ee06bf
Jim Wu <mlgmxyysd@meowcat.org>
2020-07-21 17:15:31 +0800

    Add enhancement status check

--- app/src/main/java/top/trumeet/mipush/settings/PreferenceActivity.java
+++ app/src/main/java/top/trumeet/mipush/settings/PreferenceActivity.java
@@ -1,6 +1,7 @@
-package top.trumeet.mipush.xmipushenhance.settings;
+package top.trumeet.mipush.settings;
 
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.fragment.app.FragmentActivity;
 
--- app/src/main/java/top/trumeet/mipush/settings/PreferenceFragment.java
+++ app/src/main/java/top/trumeet/mipush/settings/PreferenceFragment.java
@@ -1,31 +1,44 @@
-package top.trumeet.mipush.xmipushenhance.settings;
+package top.trumeet.mipush.settings;
 
 import android.app.AlertDialog;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.text.Html;
 import android.util.Log;
 import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import top.trumeet.mipush.xmipushenhance.R;
-import top.trumeet.mipush.xmipushenhance.settings.ini.IniConf;
-import top.trumeet.mipush.xmipushenhance.settings.ini.IniConstants;
-import top.trumeet.mipush.xmipushenhance.settings.ini.IniHelper;
-import moe.shizuku.preference.MultiSelectListPreference;
-import moe.shizuku.preference.SimpleMenuPreference;
+
+import org.meowcat.xposed.mipush.R;
+import org.meowcat.xposed.mipush.Utils;
 
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import moe.shizuku.preference.MultiSelectListPreference;
+import moe.shizuku.preference.Preference;
+import moe.shizuku.preference.SimpleMenuPreference;
+import top.trumeet.mipush.settings.ini.IniConf;
+import top.trumeet.mipush.settings.ini.IniConstants;
+import top.trumeet.mipush.settings.ini.IniUtils;
+
+import static org.meowcat.xposed.mipush.Constants.TAG;
+
 public class PreferenceFragment extends moe.shizuku.preference.PreferenceFragment implements
         SharedPreferences.OnSharedPreferenceChangeListener {
-    private static final String TAG = "PreferenceFragment";
 
     private IniConf mConf;
 
@@ -48,11 +61,15 @@ public class PreferenceFragment extends moe.shizuku.preference.PreferenceFragmen
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         getPreferenceManager().setSharedPreferencesName("settings");
         addPreferencesFromResource(R.xml.preferences);
+        Preference device_status = findPreference("device_status");
+        String status = Utils.isEnhancementEnabled() ? getString(R.string.pref_enhancement_status_success) : getString(R.string.pref_enhancement_status_failed);
+        status = String.format("%s\n\n%s", status, String.format(getString(R.string.pref_enhancement_status_summary), Build.MANUFACTURER, SystemProperties.get("ro.product.manufacturer", "failed"), SystemProperties.get("ro.product.vendor.manufacturer", "failed"), Build.BRAND, SystemProperties.get("ro.product.brand", "failed"), SystemProperties.get("ro.product.vendor.brand", "failed"), SystemProperties.get("ro.miui.ui.version.name", "failed"), SystemProperties.get("ro.miui.ui.version.code", "failed"), SystemProperties.get("ro.miui.version.code_time", "failed")));
+        device_status.setSummary(status);
     }
 
     /**
      * Render the content of ini to preferences.
-     *
+     * <p>
      * A set of well-known keys will be defined in preferences.xml.
      * For non-known keys, we will not show them through UI.
      */
@@ -93,9 +110,9 @@ public class PreferenceFragment extends moe.shizuku.preference.PreferenceFragmen
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         Log.d(TAG, "onSharedPreferenceChanged(): " + key);
         if (key.equals("module_blacklist") || key.equals("module_whitelist")) {
-            mConf.putAll(IniHelper.convertKeyToIni(key), new ArrayList<>(sharedPreferences.getStringSet(key, Collections.emptySet())));
+            mConf.putAll(IniUtils.convertKeyToIni(key), new ArrayList<>(Objects.requireNonNull(sharedPreferences.getStringSet(key, Collections.emptySet()))));
         } else {
-            mConf.put(IniHelper.convertKeyToIni(key), sharedPreferences.getString(key, null));
+            mConf.put(IniUtils.convertKeyToIni(key), sharedPreferences.getString(key, null));
         }
 
         try {
